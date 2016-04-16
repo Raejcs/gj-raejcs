3c26a33a8af11439690000b9579751964d791350
Rikard Legge <rikard.legge@gmail.com>
2016-04-16 19:11:42 +0200

    Fix 2

---
+++ Assets/Rikard/StateHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateHandler : AbstractState {
+
+	public bool isDeadly = false;
+
+	void Start(){
+		SetPropertiesByState ();
+	}
+
+	void Update () {
+		SetPropertiesByState ();
+	}
+
+	void SetPropertiesByState(){
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+		Color c = mr.material.color;
+		Color newColor = new Color(c.r,c.g,c.b,c.a);
+
+		if(GetCurrentType() == type){
+			newColor.a = .5f;
+		} else {
+			newColor.a = 1;
+		}
+
+		mr.material.color= newColor;
+	}
+}
