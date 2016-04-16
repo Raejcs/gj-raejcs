f1195bd166939686818dd39a17ade9e804a96583
Rikard Legge <rikard.legge@gmail.com>
2016-04-16 19:11:32 +0200

    Last update

---
+++ Assets/Rikard/AbstractState.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractState : MonoBehaviour {
+
+	public SOType type = SOType.Red;
+
+	private static Material blue = Resources.Load ("BlueSO", typeof(Material)) as Material;
+	private static Material red = Resources.Load ("RedSO", typeof(Material)) as Material;
+
+	// Use this for initialization
+	void Start () {
+		SetMaterialByType();
+	}
+
+	void Update () {
+		
+	}
+
+	public void SetMaterialByType(){
+		MeshRenderer mr = GetComponent<MeshRenderer> ();
+
+		switch (type) {
+		case SOType.Blue:
+			mr.material = blue;
+			break;
+		case SOType.Red:
+			mr.material = red;
+			break;
+		}
+	}
+		
+		
+	public SOType GetCurrentType(){
+		return Global.type;
+	}
+}
---
+++ Assets/Rikard/PlayerHandler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteteHandler : AbstractState {
+
+	public KeyCode toggleKey = KeyCode.S;
+
+	void Update () {
+		type = Global.type;
+
+		if (Input.GetKeyDown (toggleKey)) {
+			Global.type = GetNextType ();
+			SetMaterialByType ();
+		}
+	}
+
+	SOType GetNextType(){
+		switch (type) {
+		case SOType.Blue:
+			return SOType.Red;
+		case SOType.Red:
+			return SOType.Blue;
+		default:
+			return SOType.Red;
+		}
+	}
+}
