ff3f6d620d870250c459f707a16fea8b451ef39a
Rikard Legge <rikard.legge@gmail.com>
2016-04-17 13:13:24 +0200

    Fix collision killing

--- Assets/Rikard/PlayerHandler.cs
+++ Assets/Rikard/PlayerHandler.cs
@@ -13,13 +13,19 @@ public class PlayerHandler : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D coll){
 		var p = transform.position;
 		var p2 = coll.gameObject.transform.position;
-		var dp = p - p2;
+
+		Vector3 b2 = coll.gameObject.GetComponent<Collider2D> ().bounds.extents;
+
+		Vector3 dp = p - p2;
+
+		dp.y *= b2.x / b2.y;
 
 		var ang = Mathf.Abs(Mathf.Atan (dp.y / dp.x));
 
-		if (ang < 0.9) {
+		if (ang < 0.8) {
 			Global.restartCurrentScene ();
 		}
+		Debug.Log (ang);
 	}
 
 	void Update () {
