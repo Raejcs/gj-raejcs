b10651d72337d02dc8abcd117e04093fef79cb5e
Rikard Legge <rikard.legge@gmail.com>
2016-04-17 13:34:32 +0200

    Make jump more reliable

--- Assets/Einar/Jump.cs
+++ Assets/Einar/Jump.cs
@@ -6,53 +6,37 @@ public class Jump : MonoBehaviour {
     public bool dubbleJumpFix = true;
     int jumpCount = 0;
 
+	Rigidbody2D rigidbody;
+	BoxCollider2D collider;
+
     void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+		collider = GetComponent<BoxCollider2D> ();
 	}
 
 	bool IsGrounded() {
-		BoxCollider2D collider = GetComponent<BoxCollider2D> ();
 		Vector2 btm = new Vector2 (collider.bounds.center.x, collider.bounds.center.y - collider.bounds.extents.y-.01f);
 
 		RaycastHit2D cast = Physics2D.Raycast (btm, Vector2.down);
 		float dist = cast.fraction;
 		return dist < .1f && !cast.collider.isTrigger;
-
 	}
 	
 	void Update ()
     {
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 
-		if (Input.GetKeyDown(KeyCode.Space) && IsGrounded() || Input.GetKeyDown(KeyCode.Space) && jumpCount == 1)
-        {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			bool grounded = IsGrounded ();
 
-            if (dubbleJumpFix)
-            {
+			if (grounded) {
+				jumpCount = 0;
+			}
 
-                if (jumpCount == 1)
-                {
-                    rigidbody.velocity = Vector2.zero;
-                    rigidbody.AddForce(transform.up * jumpPower);
-                }
-                else
-                {
-                    rigidbody.AddForce(transform.up * jumpPower);
-                }
-            }
-
-            else
-            {
-                rigidbody.AddForce(transform.up * jumpPower);
-            }
-
-            jumpCount += 1;
-
-            if (jumpCount == 2) { jumpCount = 0; }
-
-            Debug.Log(jumpCount);
-            Debug.Log("jump");
-        }
-        
-
+			if (grounded || jumpCount < 2) {
+				rigidbody.velocity = Vector2.zero;
+				rigidbody.AddForce (transform.up * jumpPower);
+				jumpCount++;
+			}
+		}
 	}
 }
