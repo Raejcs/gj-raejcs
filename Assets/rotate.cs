8b5247d580f73dc5b038fe4ff7e77a870a9bfeb5
Christoffer Lundberg <ristoretto@gmail.com>
2016-04-17 14:36:17 +0200

    garpics 3

---
+++ Assets/rotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+
+    public float rotationx;
+    public float rotationy;
+    public float rotationz;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        gameObject.transform.Rotate(rotationx, rotationy, rotationz);
+
+    }
+}
